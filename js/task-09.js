88a6b7510e2b77d53c1d67a7a068abfe773b58f2
Victor Selischev <selischev.victor.kharkov@gmail.com>
2022-07-14 19:28:46 +0300

    Решает Task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const body = document.querySelector('body');
+const btnChangeColor = document.querySelector('.change-color');
+const span = document.querySelector('.color')
+
+btnChangeColor.addEventListener('click', handleChangeColorBodyClick);
+
+function handleChangeColorBodyClick(event) {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  span.textContent = color;
+}
+
