474cce74f73968764cf10ab07b4ba7eb7106e968
Victor Selischev <selischev.victor.kharkov@gmail.com>
2022-07-14 19:08:08 +0300

    Решает задачу 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,31 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', handleFormLoginSubmit);
+
+
+function handleFormLoginSubmit(event) {
+    event.preventDefault();
+    // console.log(event);
+
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    // console.log(event.currentTarget.elements.email);
+    // console.log(email);
+    // console.log(password);
+
+    if (email.value === '' || password.value === '') {
+        return alert('Все поля должны быть заполнены');
+    }
+
+    const userLogin = {};
+    userLogin.email = email.value;
+    userLogin.password = password.value;
+    console.log(userLogin);
+
+    event.currentTarget.reset();
+
+}
+
 
